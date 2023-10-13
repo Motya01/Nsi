b4fcd303c7b7bb1cad3f1e7d47b43a924c274386
Motya01 <146176370+Motya01@users.noreply.github.com>
2023-10-13 15:52:28 +0100

    Update colorpicker.js

--- colorpicker.js
+++ colorpicker.js
@@ -1,8 +1,8 @@
 const colorPicker = document.getElementById('colorPicker');
-        const selectedColor = document.getElementById('selectedColor');
+const selectedColor = document.getElementById('selectedColor');
 
-        colorPicker.addEventListener('input', (event) => {
-            const selectedValue = event.target.value;
-            selectedColor.style.backgroundColor = selectedValue;
-            selectedColor.textContent = selectedValue;
-        });
+colorPicker.addEventListener('input', (event) => {
+    const selectedValue = event.target.value;
+    selectedColor.style.backgroundColor = selectedValue;
+    selectedColor.textContent = selectedValue;
+});
