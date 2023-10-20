4688f03aaa3f7d264e918eb69f8824de6c149a69
Motya01 <146176370+Motya01@users.noreply.github.com>
2023-10-20 12:17:18 +0100

    Create loginData.js

---
+++ loginData.js
@@ -0,0 +1,14 @@
+var login = {
+    "motya": {
+        "name": "Motya",
+        "pass": "1234"
+    },
+    "motya1": {
+        "name": "Tobias",
+        "pass": "4567"
+    },
+    "motya3": {
+        "name": "Linus",
+        "pass": "7890"
+    }
+};
