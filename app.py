1c7ea2cb0cf9bda5fdde20970fe443a028dd5dd1
Motya01 <146176370+Motya01@users.noreply.github.com>
2024-09-30 08:41:25 +0000

    3009240941

--- app.py
+++ app.py
@@ -1,36 +1,20 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, jsonify, request
 import sqlite3
 
 app = Flask(__name__)
 
-# Connect to the database
 def connect_db():
-    conn = sqlite3.connect('projet_SQL/database.db')
-    conn.row_factory = sqlite3.Row  # This allows you to access rows as dictionaries
+    conn = sqlite3.connect('database.db')
     return conn
 
-# Serve the HTML file
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-# Route to execute the SQL query
-@app.route('/run_sql', methods=['POST'])
-def run_sql():
-    query = request.json.get('query')
-    try:
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute(query)
-        rows = cursor.fetchall()
-        conn.commit()
-        conn.close()
-
-        # Convert rows to a list of dictionaries
-        result = [dict(row) for row in rows]
-        return jsonify(result=result, success=True)
-    except Exception as e:
-        return jsonify(success=False, error=str(e))
+@app.route('/data', methods=['GET'])
+def get_data():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM my_table")
+    data = cursor.fetchall()
+    conn.close()
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
